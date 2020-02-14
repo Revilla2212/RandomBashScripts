a1f517029b0f588e9784f03809253a34156502cd
Alex Revilla <alexr2212@gmail.com>
2020-02-14 15:05:44 +0100

    added -t (forced total mem) option for k8 check

--- check_k8container_mem.sh
+++ check_k8container_mem.sh
@@ -4,11 +4,57 @@
 #
 # Script per activar la funcionalitat warning/critical del plugin de prometheus per memoria de contenidors k8s
 
+set -euo pipefail
+IFS=$'\n\t'
+
 usage () {
 
-    echo -e "Usage: $0 <HOSTNAME> <URL PATH> <PROTOCOL> <PORT> <CONTAINER> <WARNING> <CRITICAL>"
+    echo -e "Usage: $(basename "$0") [-h] [-H] [-u] [-P] [-p] [-f] [-w] [-c] [-t] -- Script per activar la funcionalitat warning/critical del plugin de prometheus per memoria de contenidors k8s
+    on:
+    -h  mostra aquest missatge
+    -H  indica Hostname
+    -u  indica urlpath
+    -P  indica Protocol (ex: http,https)
+    -p  indica port
+    -f  indica filtre de pod
+    -w  indica valor de warning (%)
+    -c  indica valor de critical (%)
+    -t  fixa la memoria total al valor donat
+"
     exit 1
 }
+
+if [[ $# -lt 14 ]];then
+    usage
+fi
+
+while getopts "h:t:H:u:P:p:f:w:c:" OPTION; do
+  case "$OPTION" in
+    h) usage
+       ;;
+    t) _TOTAL="$OPTARG"
+       ;;
+    H) _HOSTNAME="$OPTARG"
+       ;;
+    u) _URLPATH="$OPTARG"
+       ;;
+    P) _PROTO="$OPTARG"
+       ;;
+    p) _PORT="$OPTARG"
+       ;;
+    f) _POD="$OPTARG"
+       ;;
+    w) _WARNING="$OPTARG"
+       ;;
+    c) _CRITICAL="$OPTARG"
+       ;;
+    ?) usage
+       ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+
 
 getused () {
 
@@ -26,22 +72,34 @@ export -f getname
 
 getwarning() {
 
+    _TOTAL=${2:-}
     VAR=$(echo $1 | cut -d ':' -f2 | cut -d ';' -f1)
-    echo $VAR
+
+    if [[ "-1" -ne "$_TOTAL" ]];then
+        echo "($3/100)*$_TOTAL"|bc -l|cut -d '.' -f1
+    else
+        echo $VAR
+    fi
 }
 export -f getwarning
 
 getcritical() {
 
+    _TOTAL=${2:-}
     VAR=$(echo $1 | cut -d ':' -f3 | cut -d ';' -f1)
-    echo $VAR
+
+    if [[ "-1" -ne "$_TOTAL" ]];then
+        echo "($3/100)*$_TOTAL"|bc -l|cut -d '.' -f1
+    else
+        echo $VAR
+    fi
 }
 export -f getcritical
 
 gettotal() {
-
+    _TOTAL=${2:-}
     VAR=$(echo $1 | cut -d ':' -f3 | cut -d ';' -f3 | cut -d ' ' -f1)
-    echo $VAR
+    test  "-1" -ne "$_TOTAL" && echo $_TOTAL || echo $VAR
 }
 export -f gettotal
 
@@ -49,10 +107,10 @@ treatdata () {
 
     NAME=$(getname "$1")
     USED=$(getused "$1")
-    WARNING=$(getwarning "$1")
-    CRITICAL=$(getcritical "$1")
-    TOTAL=$(gettotal "$1")
-
+    WARNING=$(getwarning "$1" "$2" "$3")
+    CRITICAL=$(getcritical "$1" "$2" "$4")
+    TOTAL=$(gettotal "$1" "$2")
+    
     VALUE=$(echo "scale=3; ($USED/$TOTAL)*100"|bc -l)
 
     if [[ $USED -ge $CRITICAL ]];then
@@ -67,28 +125,13 @@ treatdata () {
 }
 export -f treatdata
 
-if [[ $# -ne 7 ]];then
-    usage
-fi
+#Inici de funcio principal
 
+OUTPUT=`/usr/lib/nagios/plugins/centreon-plugins/centreon_plugins.pl --plugin=cloud::prometheus::exporters::cadvisor::plugin --mode=memory --hostname=$_HOSTNAME --url-path=$_URLPATH --proto=$_PROTO --port=$_PORT --pod=$_POD --warning-usage=$_WARNING --critical-usage=$_CRITICAL 2>/dev/null`
+GLOBALMESSAGE=$(echo $OUTPUT | sed "s/'used'/\n'used'/g" | awk '{if(NR>1)print}' | xargs -I {} bash -c 'treatdata "$@"' _ {} ${_TOTAL:--1} $_WARNING $_CRITICAL)
 
-HOSTNAME=$1
-URLPATH=$2
-PROTO=$3
-PORT=$4
-POD=$5
-WARNING=$6
-CRITICAL=$7
-
-GLOBALCODE=0
-
-TPLCOMMAND="/usr/lib/nagios/plugins/centreon-plugins/centreon_plugins.pl --plugin=cloud::prometheus::exporters::cadvisor::plugin --mode=memory --hostname=$HOSTNAME --url-path=$URLPATH --proto=$PROTO --port=$PORT --pod=$POD --warning-usage=$WARNING --critical-usage=$CRITICAL"
-
-OUTPUT=$($TPLCOMMAND 2>/dev/null)
-GLOBALMESSAGE=$(echo $OUTPUT | sed "s/'used'/\n'used'/g" | awk '{if(NR>1)print}' | xargs -I {} bash -c 'treatdata "$@"' _ {})
-
-ISCRIT=$(echo -e "$GLOBALMESSAGE" | grep -w "CRITICAL")
-ISWARN=$(echo -e "$GLOBALMESSAGE" | grep -w "WARNING")
+ISCRIT=$(echo -e "$GLOBALMESSAGE" | grep -w "CRITICAL" || true)
+ISWARN=$(echo -e "$GLOBALMESSAGE" | grep -w "WARNING"|| true)
 
 CRITNAMES="CRITICAL -"
 WARNNAMES="WARNING -"
@@ -112,4 +155,4 @@ fi
 
 echo "$GLOBALMESSAGE" | xargs -I {} echo "{}" | cut -d "#" -f4 | tr '\n' ' '
 echo -e "\n"
-exit $GLOBALCODE
+exit ${GLOBALCODE:-0}
