bb5a73fda228daa2e1ed50380f9951225b9046d8
Alex Revilla <alexr2212@gmail.com>
2020-02-26 16:18:25 +0100

    deleted wphish setup

--- setupwp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-setxkbmap es
-apt-get install netlink libnl-3-dev libnl-genl-3-dev libssl-dev
-git clone https://github.com/wifiphisher/wifiphisher.git
-cd wifiphisher/
-sudo python3 setup.py install
